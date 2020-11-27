1d933fc16a5ac0fb5ad569abcda8299afd2e8f35
LuciferVerma <68379472+LuciferVerma@users.noreply.github.com>
2020-11-27 23:28:29 +0530

    Add files via upload

--- src/LuciferVerma/XMines/Main.php
+++ src/LuciferVerma/XMines/Main.php
@@ -4,44 +4,32 @@ namespace LuciferVerma\XMines;
 
 use pocketmine\Server;
 use pocketmine\Player;
-
 use pocketmine\plugin\PluginBase;
-
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
-
+use pocketmine\block\Block;
+use pocketmine\level\Level;
 use pocketmine\item\Item;
-
 use pocketmine\event\Listener;
-use pocketmine\event\server\ServerCommandEvent;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
-
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
-class main extends PluginBase implements Listener{
-    
+class Main extends PluginBase implements Listener{
+
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder()):
-        $this->saveDataFolder();
-        $this->getResource("config.yml);
-    }
-    
-    public function onDisable(){
-        $this->getLogger()->info("XMines Disabled");
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+        $this->saveDefaultConfig();
     }
     
     public function breakBlock(BlockBreakEvent $event){
-        $world = $this->getConfig()->get("world_name");
+        $world = $this->config->get("world_name");
         $player = $event->getPlayer();
         $block = $event->getBlock();
-        $blocks = $this->getConfig()->get("ore_id");
+        $blocks = $this->config->get("ore_id");
         if(!in_array($block->getId(), $blocks) && $player->getLevel()->getName() === $world){
             $event->setCancelled(true);
         }
-        
     }
-    
 }
