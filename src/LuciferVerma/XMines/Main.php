9a7cf34b43a511af9472ce6dbc55d7fc35728694
LuciferVerma <68379472+LuciferVerma@users.noreply.github.com>
2020-11-27 23:30:54 +0530

    Updated Main

---
+++ src/LuciferVerma/XMines/Main.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace LuciferVerma\XMines;
+
+use pocketmine\Server;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\block\Block;
+
+use pocketmine\level\Level;
+
+use pocketmine\item\Item;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\block\BlockBreakEvent;
+
+use pocketmine\event\block\BlockPlaceEvent;
+
+use pocketmine\utils\Config;
+
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->saveResource("config.yml");
+
+        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+
+        $this->saveDefaultConfig();
+
+    }
+
+    
+
+    public function breakBlock(BlockBreakEvent $event){
+
+        $world = $this->config->get("world_name");
+
+        $player = $event->getPlayer();
+
+        $block = $event->getBlock();
+
+        $blocks = $this->config->get("ore_id");
+
+        if(!in_array($block->getId(), $blocks) && $player->getLevel()->getName() === $world){
+
+            $event->setCancelled(true);
+
+        }
+
+    }
+
+}
